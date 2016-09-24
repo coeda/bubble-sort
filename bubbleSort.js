89d756d3dbb5b76cee43036f735e4f3025a3d7d0
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-23 20:01:04 -1000

    started working on animating bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,23 +1,44 @@
 let bubbleSort = function(array){
   let comparedNumber;
-  for (var i = 0; i < array.length; i++) {
-    if(i === 0){
-      comparedNumber = array[0];
-    } else {
-     if(comparedNumber - array[i] > 0) {
-      array[i-1] = array[i];
-      array[i] = comparedNumber;
-      i = 0;
-      comparedNumber = array [0];
-     } else {
-      comparedNumber = array[i];
-     }
+  let swapCount = 0;
+  let pass = 0;
+  let willLoop = true;
+  while(willLoop){
+    willLoop = false;
+    comparedNumber = array[0];
+    for (var i = 1; i < array.length; i++) {
+       if(comparedNumber - array[i] > 0) {
+        array[i-1] = array[i];
+        array[i] = comparedNumber;
+        willLoop = true;
+        swapCount++;
+       } else {
+        comparedNumber = array[i];
+       }
+      console.log(array);
+      console.log(swapCount);
+      pass++;
     }
-    console.log(array);
+    console.log('array after loop '+ pass + ' ' + array);
   }
-  console.log('array after loop' + array);
   return array;
 };
 
+let generateDivs = function(){
+  let container = document.getElementById('container');
+  for(let i = 0; i < 10; i++){
+    let randomNumber = Math.floor(Math.random() * 10) ;
+    let createdItem = document.createElement('div');
+    createdItem.className = 'arrayDiv';
+    createdItem.innerHTML = randomNumber;
+    createdItem.id = randomNumber.toString();
+    container.appendChild(createdItem);
+  }
+};
 
-module.exports = bubbleSort;
+let sort = function(){
+  let divList = document.querySelectorAll('.arrayDiv');
+  console.log(divList);
+};
+
+generateDivs();
--- test/bubbleSort_spec.js
+++ test/bubbleSort_spec.js
@@ -10,7 +10,9 @@ describe('bubbleSort module', () => {
   });
   it('Should return an array', () =>{
     expect(bubbleSort([4,2,6,5])).to.deep.equal([2,4,5,6]);
+    expect(bubbleSort([5,1,4,2,8])).to.deep.equal([1,2,4,5,8]);
     expect(bubbleSort([8,5,10,1,3,12])).to.deep.equal([1,3,5,8,10,12]);
+    expect(bubbleSort([12,32,156,23,153,2,1,5,23,-1,0,2])).to.deep.equal([-1,0,1,2,2,5,12,23,23,32,153,156]);
   });
 });
 
