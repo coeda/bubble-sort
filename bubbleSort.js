944f5798087c09ce3b0550bf44ea8f8590f04974
Casey Oeda <coeda@live.com>
2016-09-26 09:37:10 -1000

    completed sort on divs, need to add interval

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,3 +1,5 @@
+let randomArray = [4,5,2,1,6,7,3,8,10,9];
+
 let bubbleSort = function(array){
   let comparedNumber;
   let swapCount = 0;
@@ -17,6 +19,11 @@ let bubbleSort = function(array){
        }
       console.log(array);
       console.log(swapCount);
+      let documentDivs = document.querySelectorAll('.arrayDiv');
+      for(let i = 0; i < 10; i++){
+        documentDivs[i].style.height = (array[i] * 20) + 'px';
+        documentDivs[i].innerHTML = array[i];
+      }
       pass++;
     }
     console.log('array after loop '+ pass + ' ' + array);
@@ -24,20 +31,23 @@ let bubbleSort = function(array){
   return array;
 };
 
+
 let generateDivs = function(){
   let container = document.getElementById('container');
   for(let i = 0; i < 10; i++){
-    let randomNumber = Math.floor(Math.random() * 10) ;
+    //let randomNumber = Math.floor(Math.random() * 10) ;
     let createdItem = document.createElement('div');
     createdItem.className = 'arrayDiv';
-    createdItem.innerHTML = randomNumber;
-    createdItem.id = randomNumber.toString();
+    createdItem.innerHTML = randomArray[i];
+    createdItem.style.height = (randomArray[i] * 20) + 'px';
+    createdItem.id = i.toString();
     container.appendChild(createdItem);
   }
 };
 
 let sort = function(){
   let divList = document.querySelectorAll('.arrayDiv');
+  bubbleSort(randomArray);
   console.log(divList);
 };
 
